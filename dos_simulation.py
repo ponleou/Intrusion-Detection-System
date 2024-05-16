66341de3dfd12176691053a7339889256b753157
Ponleou Keo Sok <ponleousk@gmail.com>
2024-05-16 23:05:07 +0700

    feat: additional specifics to layers

--- dos_simulation.py
+++ dos_simulation.py
@@ -1,15 +1,19 @@
 import scapy.all as scp
 
+ip_src = input("Fake Source IP (blank for your IP): ")
 ip_dst = input("Target IP: ")
 pkg_size = input("Package Size (blank for max): ")
 
 
-def dos_simulation(ip_dst, pkg_size):
+def dos_simulation(ip_src, ip_dst, pkg_size):
     if not (pkg_size):
-        pkg_size = 65500
+        pkg_size = 65000
 
     ip_layer = scp.IP(dst=ip_dst)
-    udp_layer = scp.UDP()
+    if ip_src:
+        ip_layer = scp.IP(src=ip_src, dst=ip_dst)
+
+    udp_layer = scp.UDP(sport=80, dport=21)
     raw_layer = scp.Raw(int(pkg_size))
 
     package = ip_layer / udp_layer / raw_layer
@@ -18,4 +22,4 @@ def dos_simulation(ip_dst, pkg_size):
     print(package)
 
 
-dos_simulation(ip_dst, pkg_size)
+dos_simulation(ip_src, ip_dst, pkg_size)
