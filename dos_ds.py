87aa44f18e3a9468fdc2d0377b7dee2290d7c5a3
Ponleou Keo Sok <ponleousk@gmail.com>
2024-05-17 12:40:04 +0700

    feat: created packet sorting for unique loads

--- dos_ds.py
+++ dos_ds.py
@@ -25,18 +25,49 @@ def large_pkt_filter(packets, min_length):
     return filtered_pkt
 
 
+def pkt_with_load_filter(packets_array):
+    filtered_pkt = []
+    for pkt in packets_array:
+        try:
+            if pkt.load:
+                filtered_pkt.append(pkt)
+        except:
+            continue
+    return filtered_pkt
+
+
 # function to collect the unique loads of packet frames
-def unique_load_collector(array):
+def unique_load_pkt_sorter(packets_array):
     unique_loads = []
 
-    for x in array:
+    sorted_pkts = []
+    # array structure:
+    # [
+    #   [load_number,
+    #       [packets_with_the_same_load...]
+    #   ]...
+    # ]
+    # find the load of each sorted_pkts elements by unique_loads[load_number]
+
+    # filtering the packets with load
+    packets_with_load = pkt_with_load_filter(packets_array)
+
+    for x in packets_with_load:
         # checking for unique load of packet frames
         if x.load not in unique_loads:
             # appending the unique load value
             unique_loads.append(x.load)
-    # for x in unique_load_pkts:
-    #     unique_packet_load.append(x)
-    # return unique_load_pkts
+    # sorting unique loads with unique numbers
+    for load_number in range(len(unique_loads)):
+        sorted_pkts.append([load_number, []])
+
+    # sorting packets with the same load into one array
+    for pkt in packets_with_load:
+        for sorted_pkt in sorted_pkts:
+            if pkt.load == unique_loads[sorted_pkt[0]]:
+                sorted_pkt[1].append(pkt)
+
+    return sorted_pkt
 
 
 while True:
@@ -46,15 +77,4 @@ while True:
     time_after_sniff = dt.datetime.now()
 
     filtered_pkt = large_pkt_filter(packets, MAX_PKTFRAME_LENGTH - 200)
-    unique_pkt_loads = unique_load_collector(filtered_pkt)
-
-    # for x in filtered_pkt:
-    #     print(x.len)
-    # return [pktframe_length for pktframe_length in pktframe_length_array if pktframe_length > min_length]
-
-    # for j in range(count):
-    #     packet_load_arr.append(packet[j])
-
-    # unique(packet_load_arr)
-
-    # print(len(unique_packet_load) + packet_no_load)
+    unique_pkt_loads = unique_load_pkt_sorter(filtered_pkt)
