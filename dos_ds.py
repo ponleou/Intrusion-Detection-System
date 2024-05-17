69f2209005757c05c6fb33689c1ee7b63359ae20
Ponleou Keo Sok <ponleousk@gmail.com>
2024-05-17 10:52:25 +0700

    feat: added datetime for timing

--- dos_ds.py
+++ dos_ds.py
@@ -1,5 +1,5 @@
 import scapy.all as scp
-import numpy as np
+import datetime as dt
 
 count = 100
 
@@ -8,14 +8,13 @@ while True:
     unique_package_load = []
     package_no_load = 0
 
+    time_before_sniff = dt.datetime.now()
     package = scp.sniff(count=count)
+    time_after_sniff = dt.datetime.now()
+    print(time_after_sniff - time_before_sniff)
 
     for j in range(count):
-        try:
-            package_load_arr.append(package[j])
-        except:
-            print("Package without load:", package[j].summary())
-            package_no_load += 1
+        package_load_arr.append(package[j])
 
     def unique(list1):
         unique_list = []
