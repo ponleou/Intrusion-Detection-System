9b8fee091e862da586af7bd3c914207372aa9ca8
Ponleou Keo Sok <ponleousk@gmail.com>
2024-05-17 12:17:42 +0700

    feat: created filtering functions

--- dos_ds.py
+++ dos_ds.py
@@ -2,29 +2,59 @@ import scapy.all as scp
 import datetime as dt
 
 count = 100
+MAX_PKTFRAME_LENGTH = 1518
+
+
+# function to filter packet frames with large lengths
+def large_pkt_filter(packets, min_length):
+    filtered_pkt = []
+
+    for i in range(0, count):
+        try:
+            if packets[i].len >= min_length:
+                filtered_pkt.append(packets[i])
+        except:
+            # print(
+            #     time_after_sniff,
+            #     "Packet frame: ",
+            #     packets[i].summary(),
+            #     " no length found",
+            # )
+            continue
+
+    return filtered_pkt
+
+
+# function to collect the unique loads of packet frames
+def unique_load_collector(array):
+    unique_loads = []
+
+    for x in array:
+        # checking for unique load of packet frames
+        if x.load not in unique_loads:
+            # appending the unique load value
+            unique_loads.append(x.load)
+    # for x in unique_load_pkts:
+    #     unique_packet_load.append(x)
+    # return unique_load_pkts
+
 
 while True:
-    package_load_arr = []
-    unique_package_load = []
-    package_no_load = 0
-
+    # sniffing packets with time recorded
     time_before_sniff = dt.datetime.now()
-    package = scp.sniff(count=count)
+    packets = scp.sniff(count=count)
     time_after_sniff = dt.datetime.now()
-    print(time_after_sniff - time_before_sniff)
 
-    for j in range(count):
-        package_load_arr.append(package[j])
+    filtered_pkt = large_pkt_filter(packets, MAX_PKTFRAME_LENGTH - 200)
+    unique_pkt_loads = unique_load_collector(filtered_pkt)
 
-    def unique(list1):
-        unique_list = []
+    # for x in filtered_pkt:
+    #     print(x.len)
+    # return [pktframe_length for pktframe_length in pktframe_length_array if pktframe_length > min_length]
 
-        for x in list1:
-            if x not in unique_list:
-                unique_list.append(x)
-        for x in unique_list:
-            unique_package_load.append(x)
+    # for j in range(count):
+    #     packet_load_arr.append(packet[j])
 
-    unique(package_load_arr)
+    # unique(packet_load_arr)
 
-    print(len(unique_package_load) + package_no_load)
+    # print(len(unique_packet_load) + packet_no_load)
