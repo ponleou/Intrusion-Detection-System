662c9b66d635e9efbd8156e8647b5cfc2e9db0c1
Ponleou Keo Sok <ponleousk@gmail.com>
2024-05-17 16:48:09 +0700

    feat: created the main detector function

--- dos_ds.py
+++ dos_ds.py
@@ -3,6 +3,7 @@ import datetime as dt
 
 count = 100
 MAX_PKTFRAME_LENGTH = 1518
+threshold = 80  # percentage (lower means more sensitive)
 
 
 # function to filter packet frames with large lengths
@@ -25,6 +26,7 @@ def large_pkt_filter(packets, min_length):
     return filtered_pkt
 
 
+# filtering packets with loads
 def pkt_with_load_filter(packets_array):
     filtered_pkt = []
     for pkt in packets_array:
@@ -38,6 +40,7 @@ def pkt_with_load_filter(packets_array):
 
 # function to collect the unique loads of packet frames
 def unique_load_pkt_sorter(packets_array):
+    # contains all the unique loads
     unique_loads = []
 
     sorted_pkts = []
@@ -51,6 +54,7 @@ def unique_load_pkt_sorter(packets_array):
 
     # filtering the packets with load
     packets_with_load = pkt_with_load_filter(packets_array)
+    total_pkts = len(packets_with_load)
 
     for x in packets_with_load:
         # checking for unique load of packet frames
@@ -67,7 +71,28 @@ def unique_load_pkt_sorter(packets_array):
             if pkt.load == unique_loads[sorted_pkt[0]]:
                 sorted_pkt[1].append(pkt)
 
-    return sorted_pkt
+    return unique_loads, sorted_pkts, total_pkts
+
+
+def heavy_packet_dos_detector(
+    unique_pkt_loads, sorted_pkt_loads, total_pkts, threshold
+):
+    unique_load_test = len(unique_pkt_loads) <= total_pkts * (
+        1 - (threshold / 100)
+    )  # if unique loads is too little
+
+    same_pkt_load_test = False  # if there is too many packets with the same load
+    detected_pkt_load = []  # stores the load that is within multiple packets
+
+    for sorted_pkt_load in sorted_pkt_loads:
+        if len(sorted_pkt_load[1]) >= total_pkts * (threshold / 100):
+            same_pkt_load_test = True
+            detected_pkt_load.append(unique_pkt_loads[sorted_pkt_load[0]])
+
+    if unique_load_test or same_pkt_load_test:
+        print(
+            dt.datetime.now(), "Potential DoS/DDoS Attack Detected: heavy packet flood"
+        )
 
 
 while True:
@@ -76,5 +101,14 @@ while True:
     packets = scp.sniff(count=count)
     time_after_sniff = dt.datetime.now()
 
-    filtered_pkt = large_pkt_filter(packets, MAX_PKTFRAME_LENGTH - 200)
-    unique_pkt_loads = unique_load_pkt_sorter(filtered_pkt)
+    # filter large packets
+    filtered_pkt = large_pkt_filter(packets, MAX_PKTFRAME_LENGTH * (threshold / 100))
+
+    # only scans when there are enough large packets in a loop
+    if len(filtered_pkt) >= count * (threshold / 100):
+        unique_pkt_loads, sorted_pkt_loads, total_pkts = unique_load_pkt_sorter(
+            filtered_pkt
+        )
+        heavy_packet_dos_detector(
+            unique_pkt_loads, sorted_pkt_loads, total_pkts, threshold
+        )
