c45ad1acf83221f46750f7191245a4069483116b
Sok Keo Ponleou <ponleousk@gmail.com>
2024-05-22 21:44:03 +0700

    feat: completed SYN/ACK finding

--- synflood_ids_multithreading.py
+++ synflood_ids_multithreading.py
@@ -7,8 +7,12 @@ timeout = 2  # seconds
 
 # temporary (until we integrate flag check in sniff())
 def check_flag(packet, flag):
-    if packet.getlayer(scp.TCP).flags == flag:
-        return packet
+    # try and except because some tcp packets dont have flags
+    try:
+        if packet.getlayer(scp.TCP).flags == flag:
+            return packet
+    except:
+        pass
 
     return None
 
@@ -20,6 +24,8 @@ def start_syn_ack_thread(packet):
     if not check_flag(packet, "S"):
         return
 
+    print("SYN found (1)")
+
     thread = threading.Thread(target=find_syn_ack_thread, args=(packet,))
     thread.start()
 
@@ -30,11 +36,21 @@ def find_syn_ack_thread(packet):
     packet_dst = packet.getlayer(scp.IP).dst
 
     # sniff to find SYN/ACK
-    scp.sniff(
+    packets = scp.sniff(
         filter="tcp and src host " + packet_dst + " and dst host " + packet_src,
         prn=find_syn_ack,
         timeout=timeout,
     )
+
+    # TEMPORARY: can remove after adding flag filters to sniff
+    # checking to see if theres any SYN/ACK after timeout
+    syn_ack_packet_found = False
+    for packet in packets:
+        if check_flag(packet, "SA"):
+            syn_ack_packet_found = True
+
+    if not syn_ack_packet_found:
+        print("No SYN/ACK packet found after timeout")
 
 
 # executed in thread (1)
@@ -43,7 +59,7 @@ def find_syn_ack(packet):
     if not check_flag(packet, "SA"):
         return
 
-    print(datetime.now(), "SYN/ACK found")
+    print(datetime.now(), "SYN/ACK found (2)")
 
 
 scp.sniff(prn=start_syn_ack_thread, filter="tcp")
