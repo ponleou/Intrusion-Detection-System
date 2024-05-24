a35c73b292c333d85d74531fbafb35ab1bb4bf8e
Sok Keo Ponleou <ponleousk@gmail.com>
2024-05-24 17:51:55 +0700

    docs: added port scan attack simulation

---
+++ portscan.py
@@ -0,0 +1,25 @@
+import scapy.all as scp
+import random
+
+source_ip = input("Fake Source IP (blank for random IP): ")
+target_ip = input("Target IP: ")
+start_port = input("Start port: ")
+end_port = input("End port: ")
+
+MAX_PORT = 65535
+
+
+def port_scan(source_ip, target_ip, start_port, end_port):
+    packets = []
+    for port in range(int(start_port), int(end_port) + 1):
+        ip_layer = scp.IP(src=scp.RandIP(), dst=target_ip)
+
+        if source_ip:
+            ip_layer.src = source_ip
+
+        packet = ip_layer / scp.TCP(dport=port, flags="S")
+        packets.append(packet)
+    scp.send(packets)
+
+
+port_scan(source_ip, target_ip, start_port, end_port)
